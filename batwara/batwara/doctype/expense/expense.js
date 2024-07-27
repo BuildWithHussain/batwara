d74450a7d2b75ec5d1db54b2f1dad73843e7ea15
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-07-27 08:48:56 +0530

    chore: expense doctype

---
+++ batwara/batwara/doctype/expense/expense.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, Build With Hussain and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Expense", {
+	onload(frm) {
+		frm.set_query("paid_by", function(){
+			return {
+				filters: {
+					"ignore_user_type": 1
+				}
+			}
+		});
+	},
+});
---
+++ batwara/batwara/doctype/expense_split/expense_split.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Build With Hussain and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ExpenseSplit(Document):
+	pass
