f7a4a4cf064c6ee35645defa0d09a639f8bb221a
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-07-27 09:10:06 +0530

    feat: add frappe-ui starter

--- batwara/hooks.py
+++ batwara/hooks.py
@@ -230,3 +230,5 @@ export_python_type_annotations = True
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+
+website_route_rules = [{'from_route': '/frontend/<path:app_path>', 'to_route': 'frontend'},]
---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import frappeui from 'frappe-ui/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [frappeui(), vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: `../${path.basename(path.resolve('..'))}/public/frontend`,
+    emptyOutDir: true,
+    target: 'es2015',
+  },
+  optimizeDeps: {
+    include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
+  },
+})
