3c0b9fe1b02d8078ab0febe2eec10dfa0a246e3d
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-07-28 15:54:09 +0530

    refactor: extract out navbar component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,41 +10,13 @@
     <router-view />
   </div>
 
-  <nav v-if="session.isLoggedIn" class="absolute bottom-0 w-full bg-gray-100 py-2 px-8">
-	<ul class="flex justify-around items-center">
-		<li>
-			<RouterLink :to="{name: 'Dashboard'}" v-slot="{ isExactActive }">
-				<FeatherIcon
-					:class="[
-						'w-6',
-						isExactActive ? 'text-gray-700' : 'text-gray-500'
-					]"
-					name="home" />
-			</RouterLink>
-		</li>
-
-		<li>
-			<Button :route="{ name: 'CreateExpense' }" theme="green" variant="solid" size="xl" icon="plus" />
-		</li>
-
-		<li>
-			<RouterLink :to="{name: 'Friends'}" v-slot="{ isExactActive }">
-				<FeatherIcon
-					:class="[
-						'w-6',
-						isExactActive ? 'text-gray-700' : 'text-gray-500'
-					]"
-					name="users" />
-			</RouterLink>
-		</li>
-	</ul>
-  </nav>
+  <BottomNavBar />
 </template>
 
 <script setup>
 import { computed, inject, provide, watch } from "vue";
-import { FeatherIcon } from "frappe-ui";
 import { getFriendsListResource } from "@/data/friends";
+import BottomNavBar from "./components/layout/BottomNavBar.vue";
 
 const session = inject("session");
 const friendsList = getFriendsListResource();
---
+++ frontend/src/components/layout/BottomNavBar.vue
@@ -0,0 +1,38 @@
+<template>
+	  <nav v-if="session.isLoggedIn" class="absolute bottom-0 w-full bg-gray-100 py-2 px-8">
+	<ul class="flex justify-around items-center">
+		<li>
+			<RouterLink :to="{name: 'Dashboard'}" v-slot="{ isExactActive }">
+				<FeatherIcon
+					:class="[
+						'w-6',
+						isExactActive ? 'text-gray-700' : 'text-gray-500'
+					]"
+					name="home" />
+			</RouterLink>
+		</li>
+
+		<li>
+			<Button :route="{ name: 'CreateExpense' }" theme="green" variant="solid" size="xl" icon="plus" />
+		</li>
+
+		<li>
+			<RouterLink :to="{name: 'Friends'}" v-slot="{ isExactActive }">
+				<FeatherIcon
+					:class="[
+						'w-6',
+						isExactActive ? 'text-gray-700' : 'text-gray-500'
+					]"
+					name="users" />
+			</RouterLink>
+		</li>
+	</ul>
+  </nav>
+</template>
+
+<script setup>
+import {inject} from "vue";
+import { FeatherIcon } from "frappe-ui";
+
+const session = inject("session");
+</script>
