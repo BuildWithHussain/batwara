fc4f9dc6b94484336bb7f1e5d99054a1ad2132aa
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-07-28 15:39:25 +0530

    fix: don't show bottom bar before login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,7 +10,7 @@
     <router-view />
   </div>
 
-  <nav class="absolute bottom-0 w-full bg-gray-100 py-2 px-8">
+  <nav v-if="session.isLoggedIn" class="absolute bottom-0 w-full bg-gray-100 py-2 px-8">
 	<ul class="flex justify-around items-center">
 		<li>
 			<RouterLink :to="{name: 'Dashboard'}" v-slot="{ isExactActive }">
